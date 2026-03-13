63403a899cf6656018c3c39cd9602f1db38620c6
kakurokan <leokakali7@gmail.com>
2026-03-13 16:07:37 +0000

    Add initial implementation of DNS message encoding

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+from dataclasses import dataclass
+import dataclasses
+import struct
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+random.seed(1)
+TYPE_A = 1
+CLASS_IN = 1
+
+@dataclass
+class DNSHeader:
+    id: int
+    flags: int
+    questions: int = 0
+    answers: int = 0
+    authorities: int = 0
+    additions: int = 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@dataclass
+class DNSQuestion:
+    name: bytes
+    type_: int
+    class_: int
+
+def header_to_bytes(header: DNSHeader):
+    fields = dataclasses.astuple(header)
+    #O 6 H's correspondem aos 6 campos do DNSHeader
+    #O '!' ordena que os dados sejam empacotados no formato padrão oficial das redes internet
+    return struct.pack("!HHHHHH", *fields)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def question_to_byter(question: DNSQuestion):
+    return question.name + struct.pack("!HH", question.type_, question.class_)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#Exemplo: “google.com” é traduzido para b"\x06google\x03com\x00"
+def encode_dns_name(domain_name):
+    encoded = b""
+    for part in domain_name.encode("ascii").split(b"."):
+        encoded += bytes([len(part)]) + part
+    return encoded + b"\x00"
